aa9649b269a64a0abd9d53b3b84a1f0f759699a2
TOURNEUX Elias <tourneux@proton.me>
2023-04-29 12:02:48 +0200

    Add login page

--- Fleurs/ConnectionWindow.xaml.cs
+++ Fleurs/ConnectionWindow.xaml.cs
@@ -18,6 +18,7 @@ using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using MySql.Data.MySqlClient;
+using System.Diagnostics;
 
 namespace Fleurs
 {
@@ -39,31 +40,53 @@ namespace Fleurs
 
         private void Connection_Button_Click(object sender, RoutedEventArgs e)
         {
-            //connection.Open();
-            string email = Email_TextBox.Text;
-            MySqlCommand command = connection.CreateCommand();
-            command.CommandText = $"SELECT Count(*) FROM clients WHERE email='{email}';";
-            MySqlDataReader reader = command.ExecuteReader();
-            string found = "-1";
-            while (reader.Read())
+            try
             {
-                found = reader.GetString(0);
-                //Email_TextBox.Text = found;
-            }
-            reader.Close();
-            if (found == "1")
-            {
-                MessageBox.Show("It works!", "Super", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            else
-            {
-                var result=MessageBox.Show("Voulez vous créer un compte ?", "Votre email n'est pas dans notre systeme!", MessageBoxButton.YesNo, MessageBoxImage.Warning);
-                if (result == MessageBoxResult.Yes)
+                //connection.Open();
+                string email = Email_TextBox.Text;
+                string password = Mdp_TextBox.Password;
+                Trace.WriteLine(password);
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = $"SELECT * FROM clients WHERE email='{email}';";
+                MySqlDataReader reader = command.ExecuteReader();
+                int compteur = 0;
+                while (reader.Read())
                 {
-                    Creation_Compte creation_Compte = new Creation_Compte();
-                    this.Content = creation_Compte; 
+                    compteur++;
+                    try
+                    {
+                        string password_mysql = (string)reader["password"];
+                        string name_mysql = (string)reader["name"];
+
+                        if (password_mysql == password)
+                        {
+                            MessageBox.Show("Bonjour " + name_mysql + " ! Bienvenue sur l'extranet de Chez Rose.", "Vous êtes connecté", MessageBoxButton.OK, MessageBoxImage.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Le mot de passe ne correspond pas au compte spécififé.\nPour rénitialiser votre mot de passe, veuillez contacter votre fleuriste.", "Mot de passe incorrect", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine(ex);
+                    }
                 }
+                reader.Close();
+                if (compteur == 0)
+                {
+                    var result = MessageBox.Show("Le compte associé à "+email+" n'existe pas sur notre extranet.\nSouhaitez-vous créer un compte avec cette adresse e-mail ?", "Compte inconnu", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        Creation_Compte creation_Compte = new Creation_Compte();
+                        this.Content = creation_Compte;
+                    }
+                }
+            
+            } catch (Exception ex)
+            {
+                Trace.WriteLine(ex);
             }
-        }
+         }
     }
 }
