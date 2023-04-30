ee243914075f220819f64a4ea9f1689510cb66f7
Marc94440 <marc.viallard@edu.devinci.fr>
2023-04-30 22:11:38 +0200

    Finition de la commande standard (Il reste juste a voir les adresses et vérifier ce que j'envoie dans la bd)

--- Fleurs/Windows/Choix_Bouquet_Standard.xaml.cs
+++ Fleurs/Windows/Choix_Bouquet_Standard.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialDesignThemes.Wpf;
+using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -7,6 +9,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -22,16 +25,23 @@ namespace Fleurs.Windows
     /// </summary>
     public partial class Choix_Bouquet_Personnalise : UserControl
     {
-        private string emailPage;
-        private string typePage;
-        private string statutPage;
+        string emailPage;
+        string typePage;
+        string statutPage;
+        string bouquet;
+
+        string connectionString;
+        MySqlConnection connection;
 
         public Choix_Bouquet_Personnalise(string email, string type)
         {
             emailPage = email;
             typePage= type;
-            InitializeComponent(); 
-            ;
+            InitializeComponent();
+            connectionString = "SERVER=marc.eliqs.dev;DATABASE=Fleurs;UID=marc;PASSWORD=marcgroszizi1789;";
+            //connectionString = "SERVER=localhost;PORT=3306;DATABASE=Fleurs;UID=root;PASSWORD=root;";
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
         }
 
         private void FinaliseCommande_Button_Click(object sender, RoutedEventArgs e)
@@ -42,40 +52,92 @@ namespace Fleurs.Windows
             string dureeEnJ_str;
             int dureeEnJ_int;
             bool futur = !(selectedDate < date_du_jour);
-            if (selectedDate.HasValue)
+            string bouquet_choisi;
+            string dateDeLivraison;
+            if (selectedDate.HasValue && !string.IsNullOrEmpty(Choix_ComboBox.Text))
             {
+                dateDeLivraison = selectedDate.Value.ToString("yyyy-MM-dd");
                 duree = selectedDate - date_du_jour;
                 dureeEnJ_str = duree.Value.ToString("dd");
                 dureeEnJ_int = Convert.ToInt32(dureeEnJ_str) + 1;
                 futur = !(selectedDate < date_du_jour);
-                if (futur && dureeEnJ_int>=3)
+                bouquet_choisi = Choix_ComboBox.Text;
+                int prix;
+                switch(bouquet_choisi)
                 {
-                    Finalisation_Commande fin_de_commande = new Finalisation_Commande(emailPage, typePage);
-                    this.Content = fin_de_commande;
-                    //MessageBox.Show("Commande Validé", "Good", MessageBoxButton.OK, MessageBoxImage.Information);
+                    case "Gros Merci":
+                        prix = 45;
+                        break;
+                    case "L’amoureux":
+                        prix = 65;
+                        break;
+                    case "L’Exotique":
+                        prix = 40;
+                        break;
+                    case "Maman Arrangement":
+                        prix = 80;
+                        break;
+                    case "Vive la mariée":
+                        prix = 120;
+                        break;
+                    default:
+                        prix = -1;
+                        break;
+                        ;
+                }
+                bool en_stock = false;
+                MySqlCommand command = connection.CreateCommand();
+                command.CommandText = $"SELECT Count(*) FROM bouquets WHERE stock> 0 AND name = '{bouquet_choisi}';";
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    en_stock = reader.GetBoolean(0);//  GetString(0);
+                }
+                reader.Close();
+                if (futur && dureeEnJ_int>=3)
+                {                   
+                    if(en_stock)
+                    {
+                        Finalisation_Commande fin_de_commande = new Finalisation_Commande(emailPage, typePage, bouquet_choisi, "CC",dateDeLivraison, prix);
+                        this.Content = fin_de_commande;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Malheuresement, il ne nous reste plus de "+ bouquet_choisi, "Plus de stock", MessageBoxButton.OK, MessageBoxImage.Information);
+                    }
                 }
                 else if (dureeEnJ_int <=2 && futur)
                 {
-                    if (MessageBox.Show("Votre commande à été effectué moins de trois jours avant la date de livraison. Il est possible que nous ayons des problèmes de stocks. Voulez vous quand même poursuivre votre commande ?", "Risque de pénurie", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
+                    if (en_stock)
                     {
-                        if (1==2)
+                        if (MessageBox.Show("Votre commande à été effectué moins de trois jours avant la date de livraison. Il est possible que nous ayons des problèmes de stocks. Voulez vous quand même poursuivre votre commande ?", "Risque de pénurie", MessageBoxButton.YesNo, MessageBoxImage.Information) == MessageBoxResult.Yes)
                         {
-
+                            Finalisation_Commande fin_de_commande = new Finalisation_Commande(emailPage, typePage, bouquet_choisi, "VINV", dateDeLivraison, prix);
+                            this.Content = fin_de_commande;
                         }
-                        Finalisation_Commande fin_de_commande = new Finalisation_Commande(emailPage, typePage);
-                        this.Content = fin_de_commande;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Malheuresement, il ne nous reste plus de " + bouquet_choisi, "Plus de stock", MessageBoxButton.OK, MessageBoxImage.Information);
                     }
                 }
-                else { MessageBox.Show("Veuillez choisir une date de livraison dans le futur", "Date de livraison incorrect", MessageBoxButton.OK, MessageBoxImage.Information); }
+                else { MessageBox.Show("Veuillez choisir une date de livraison dans le futur", "Date de livraison incorrect", MessageBoxButton.OK, MessageBoxImage.Warning); }
                 
             }
-            else{MessageBox.Show("Veuillez choisir une date de livraison", "Pas de date de livraison", MessageBoxButton.OK, MessageBoxImage.Information);}
+            else if (!selectedDate.HasValue)
+            {
+                MessageBox.Show("Veuillez choisir une date de livraison", "Pas de date de livraison", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+            else
+            {
+                MessageBox.Show("Veuillez choisir un bouquet", "Pas de bouquet", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
         }
 
         private void Retour_Button_Click(object sender, RoutedEventArgs e)
         {
             Choix_Perso_standard choix_du_type_de_bouquet = new Choix_Perso_standard(emailPage);
-            this.Content = choix_du_type_de_bouquet;//Peut être utiliser le même qu'au début
+            this.Content = choix_du_type_de_bouquet;
         }
     }
 }
--- Fleurs/Windows/Finalisation_Commande.xaml.cs
+++ Fleurs/Windows/Finalisation_Commande.xaml.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Fleurs.Windows
 {
@@ -20,13 +23,34 @@ namespace Fleurs.Windows
     /// </summary>
     public partial class Finalisation_Commande : UserControl
     {
-        private string emailPage;
-        private string typePage;
-        public Finalisation_Commande(string email, string type)
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+        string emailPage;
+        string typePage;
+        string bouquetPage;
+        string etatPage;
+        string dateDeLivraisonPage;
+        int prixPage;
+        string Message;
+
+        string connectionString;
+        MySqlConnection connection;
+        public Finalisation_Commande(string email, string type, string bouquet, string etat, string dateDeLivraison, int prix)
         {
             emailPage = email;
             typePage = type;
+            bouquetPage = bouquet;
+            etatPage = etat;
+            dateDeLivraisonPage = dateDeLivraison;
+            prixPage = prix;
             InitializeComponent();
+            connectionString = "SERVER=marc.eliqs.dev;DATABASE=Fleurs;UID=marc;PASSWORD=marcgroszizi1789;";
+            //connectionString = "SERVER=localhost;PORT=3306;DATABASE=Fleurs;UID=root;PASSWORD=root;";
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
         }
 
         private void Retour_Button_Click(object sender, RoutedEventArgs e)
@@ -41,6 +65,24 @@ namespace Fleurs.Windows
                 Choix_Bouquet_Personnalisee choix_standard = new Choix_Bouquet_Personnalisee(emailPage, typePage);
                 this.Content = choix_standard;
             }
+        }
+    private void Payement_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Message = Message_TextBox.Text;
+            
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = $"UPDATE bouquets " +
+                $"SET stock = stock-1 " +
+                $"WHERE name='{bouquetPage}';";
+            MySqlDataReader reader = command.ExecuteReader();
+            reader.Close();
+            command.CommandText = $"INSERT INTO orders " +
+                $"(address, delivery, max_price, message, status, type) " +
+                $"VALUES(2,'{dateDeLivraisonPage}',{prixPage},'{Message}','{etatPage}','{typePage}');";//voir pour les address
+            reader = command.ExecuteReader();
+            reader.Close();
+            //juste ça 
+            // et relire et commenter
         }
     }
 }
